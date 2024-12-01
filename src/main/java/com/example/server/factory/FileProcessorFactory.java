2fd9b7568642c91298c97c5b0794f15a9f08e0d9
Steenq <vladi.bruek@gmail.com>
2024-12-02 01:42:05 +0300

    NewRead

--- src/main/java/com/example/server/factory/FileProcessorFactory.java
+++ src/main/java/com/example/server/factory/FileProcessorFactory.java
@@ -2,6 +2,7 @@ package com.example.server.factory;
 
 import com.example.server.processor.FileProcessor;
 import com.example.server.processor.TxtFileProcessor;
+import com.example.server.processor.XmlFileProcessor;
 
 public class FileProcessorFactory {
 
@@ -9,6 +10,8 @@ public class FileProcessorFactory {
         switch (fileType.toLowerCase()) {
             case "txt":
                 return new TxtFileProcessor();
+            case "xml":
+                return new XmlFileProcessor();
             default:
                 throw new IllegalArgumentException("Unsupported file type: " + fileType);
         }
--- src/main/java/com/example/server/processor/XmlFileProcessor.java
+++ src/main/java/com/example/server/processor/XmlFileProcessor.java
@@ -1,4 +1,9 @@
 package com.example.server.processor;
 
-public class XmlFileProcessor {
+public class XmlFileProcessor implements FileProcessor{
+    @Override
+    public String process(String content) {
+        // Логика обработки текста
+        return content;
+    }
 }
